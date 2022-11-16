439979e1d5cd52f02bec2638fb9334e511417342
logger421 <wojteklotko3@gmail.com>
2022-11-16 12:18:45 +0100

    Implemented all functions, they are working but for sure for further refactor.

--- Zadania 5/fracs.py
+++ Zadania 5/fracs.py
@@ -2,11 +2,11 @@ def add_frac(frac1, frac2):
     if frac1[1] != frac2[1]:
         m = nww(frac1[1], frac2[1])
         if frac1[1] != m and frac2[1] == m:
-            return [frac1[0] * (m/frac1[1]) + frac2[0], m]
+            return [frac1[0] * (m / frac1[1]) + frac2[0], m]
         elif frac1[1] == m and frac2[1] != m:
-            return [frac1[0] + frac2[0] * (m/frac2[1]), m]
+            return [frac1[0] + frac2[0] * (m / frac2[1]), m]
         else:
-            return [frac1[0] * (m/frac1[1]) + frac2[0] * (m/frac2[1]), m]
+            return [frac1[0] * (m / frac1[1]) + frac2[0] * (m / frac2[1]), m]
     else:
         return [frac1[0] + frac2[0], frac1[1]]
 
@@ -15,25 +15,28 @@ def sub_frac(frac1, frac2):
     if frac1[1] != frac2[1]:
         m = nww(frac1[1], frac2[1])
         if frac1[1] != m and frac2[1] == m:
-            return [frac1[0] * (m/frac1[1]) - frac2[0], m]
+            return [frac1[0] * (m / frac1[1]) - frac2[0], m]
         elif frac1[1] == m and frac2[1] != m:
-            return [frac1[0] - frac2[0] * (m/frac2[1]), m]
+            return [frac1[0] - frac2[0] * (m / frac2[1]), m]
         else:
-            return [frac1[0] * (m/frac1[1]) - frac2[0] * (m/frac2[1]), m]
+            return [frac1[0] * (m / frac1[1]) - frac2[0] * (m / frac2[1]), m]
     else:
         return [frac1[0] - frac2[0], frac1[1]]
 
 
 def mul_frac(frac1, frac2):
-    return [frac1[0]*frac2[0], frac1[1]*frac2[1]]
+    return [frac1[0] * frac2[0], frac1[1] * frac2[1]]
 
 
 def div_frac(frac1, frac2):
-    return mul_frac(frac1, [frac2[1], frac2[0]])
+    if not is_zero(frac2):
+        return mul_frac(frac1, [frac2[1], frac2[0]])
+    else:
+        raise ZeroDivisionError
 
 
 def is_positive(frac):
-    return True if frac[0]*frac[1] > 0 else False
+    return True if frac[0] * frac[1] > 0 else False
 
 
 def is_zero(frac):
@@ -41,11 +44,28 @@ def is_zero(frac):
 
 
 def cmp_frac(frac1, frac2):
-    pass  # -1 | 0 | +1
+    def wsp_mian(f1, f2):
+        m = nww(f1[1], f2[1])
+        f1 = [f1[0] * (m / f1[1]), m]
+        f2 = [f2[0] * (m / f2[1]), m]
+        return f1, f2
+
+    frac1, frac2 = wsp_mian(frac1, frac2)
+    print(frac1)
+
+    if frac1[0] > frac2[0]:
+        return -1
+    elif frac1[0] == frac2[0]:
+        return 0
+    else:
+        return 1
 
 
 def frac2float(frac):
-    pass  # konwersja do float
+    if frac[1] != 0:
+        return float(frac[0] / frac[1])
+    else:
+        raise ZeroDivisionError
 
 
 def nww(a, b):
@@ -93,9 +113,11 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(True, is_zero([0, 1]))
         self.assertEqual(False, is_zero([1, 2]))
 
-    def test_cmp_frac(self): pass
+    def test_cmp_frac(self):
+        self.assertEqual(-1, cmp_frac([1, 2], [1, 4]))
 
-    def test_frac2float(self): pass
+    def test_frac2float(self):
+        self.assertEqual(0.5, frac2float([1,2]))
 
     def tearDown(self):
         print("Tests in progress...")
