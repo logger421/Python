e56f6d9e1bd6d3317e0fda31bf415dad45cdf489
logger421 <wojteklotko3@gmail.com>
2022-11-27 16:09:55 +0100

    Added tests for rectangles class.

---
+++ Zadania7/test_rectangles.py
@@ -0,0 +1,41 @@
+import unittest
+from rectangles import Rectangle
+from Zadania6.points import Point
+
+
+class TestPoint(unittest.TestCase):
+    def setUp(self) -> None:
+        self.r1 = Rectangle(0, 0, 1, 1)
+
+    def tearDown(self) -> None:
+        del self.r1
+
+    def test_str(self):
+        self.assertEqual(str(self.r1), "[(0, 0), (1, 1)]")
+
+    def test_repr(self):
+        self.assertEqual(repr(self.r1), "Rectangle(0, 0, 1, 1)")
+
+    def test_eq(self):
+        r2 = Rectangle(0, 0, 1, 1)
+        self.assertTrue(self.r1 == r2)
+        r2 = Rectangle(0, 0, 2, 2)
+        self.assertFalse(self.r1 == r2)
+
+    def test_neq(self):
+        r2 = Rectangle(0, 0, 2, 2)
+        self.assertTrue(self.r1 != r2)
+
+    def test_center(self):
+        self.assertEqual(self.r1.center(), Point(0.5, 0.5))
+
+    def test_area(self):
+        self.assertEqual(self.r1.area(), 1)
+
+    def test_move(self):
+        self.r1.move(1, 1)
+        self.assertTrue(self.r1 == Rectangle(1, 1, 2, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
