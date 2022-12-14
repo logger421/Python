4bfd837b9d2c0a7b7ddbe9aa24cca1059fbdd29f
logger421 <wojteklotko3@gmail.com>
2022-12-14 22:09:17 +0100

    Added remove_tail and join methods

--- Zadania 9/single_list.py
+++ Zadania 9/single_list.py
@@ -17,6 +17,22 @@ class SingleList:
         self.head = None
         self.tail = None
 
+    def __iter__(self):
+        # Przy tworzeniu iteratora trzeba mieć zmienną, która będzie pamiętać stan.
+        # Przy kolejnym tworzeniu iteratora będzie ustawianie na początek.
+        self.current = self.head
+        return self
+
+    def __next__(self):
+        if self.current:
+            node = self.current
+            self.current = self.current.next
+            return node.data
+        else:  # self.current is None
+            raise StopIteration
+
+    next = __next__  # kompatybilność Py2 i Py3
+
     def is_empty(self):
         # return self.length == 0
         return self.head is None
@@ -52,3 +68,58 @@ class SingleList:
         self.length -= 1
         return node  # zwracamy usuwany node
 
+    def remove_tail(self):
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.head
+        if self.tail == self.head:  # lista z jednym elementem
+            tmp = self.tail
+            self.head = self.tail = None
+            self.length -= 1
+            return tmp
+        else:
+            while node.next != self.tail:
+                node = node.next
+            tmp = self.tail
+            node.next = None
+            self.tail = node
+        self.length -= 1
+        return tmp
+
+    # Zwraca cały węzeł, skraca listę.
+    # Dla pustej listy rzuca wyjątek ValueError.
+
+    def join(self, other):
+        if other.is_empty():
+            raise ValueError("pusta lista")
+        while not other.is_empty():
+            self.insert_tail(other.remove_tail())
+
+    def clear(self):
+        pass  # czyszczenie listy
+
+def print_list(l):
+    for item in l:
+        print(item)
+
+
+if __name__ == '__main__':
+    alist = SingleList()
+    alist.insert_head(Node(11))  # [11]
+    alist.insert_head(Node(22))  # [22, 11]
+    alist.insert_head(Node(33))  # [33, 22, 11]
+
+    jointList = SingleList()
+    jointList.insert_head(Node(1))
+    jointList.insert_head(Node(2))
+    jointList.insert_head(Node(3))  # [3, 2, 1]
+
+    alist.join(jointList)
+
+    print('first list after join: ')
+    # kolejność 22, 11, 33
+    print_list(alist)
+
+    print('second list after join:')
+    # kolejność 22, 11, 33, 1, 2, 3
+    print_list(jointList)
---
+++ Zadania 9/single_list_test.py
@@ -0,0 +1,2 @@
+import pytest
+import single_list
