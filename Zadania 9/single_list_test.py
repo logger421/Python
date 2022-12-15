5ac5665b31258d71f9985205f431433692431646
logger421 <wojteklotko3@gmail.com>
2022-12-15 19:44:02 +0100

    Added tests.

--- Zadania 9/single_list_test.py
+++ Zadania 9/single_list_test.py
@@ -3,6 +3,19 @@ from single_list import Node
 from single_list import SingleList
 
 
+def test_eq():
+    l1 = SingleList()
+    l1.insert_head(Node(11))
+    l1.insert_head(Node(12))
+    l1.insert_head(Node(13))
+
+    l2 = SingleList()
+    l2.insert_head(Node(11))
+    l2.insert_head(Node(12))
+    l2.insert_head(Node(13))
+
+    assert l1 == l2
+
 def test_empty_list():
     l = SingleList()
     assert l.is_empty() == True
@@ -30,17 +43,43 @@ def test_insert():
     assert alist.head == alist.tail
 
     alist.insert_head(Node(22))
-    assert alist.head.data == 22
+    assert alist.head == Node(22)
 
     alist.insert_tail(Node(33))
-    assert alist.tail.data == 33
+    assert alist.tail == Node(33)
     assert alist.head != alist.tail
 
 def test_remove():
-    pass
+    alist = SingleList()
+    alist.insert_head(Node(11))
+    alist.insert_head(Node(22))
+    alist.insert_head(Node(33))
+    assert alist.remove_head() == Node(33)
+    assert alist.remove_tail() == Node(11)
 
 def test_join():
-    pass
+    alist = SingleList()
+    alist.insert_head(Node(11))
+    alist.insert_head(Node(22))
+    alist.insert_head(Node(33))
+
+    join_test = SingleList()
+    join_test.insert_head(Node(1))
+    join_test.insert_head(Node(2))
+    join_test.insert_head(Node(3))
+
+    alist.join(join_test)
+    assert alist == create_list([33, 22, 11, 3 , 2, 1])
+    assert join_test.count() == 0
+    assert join_test.is_empty() == True
+
+
+def create_list(l):
+    my_list = SingleList()
+    for el in l:
+        my_list.insert_tail(Node(el))
+    return my_list
+
 
 if __name__ == "__main__":
     pytest.main()
