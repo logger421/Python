cb52f2ee1efaa9246e31967cda38ed67c018c6bc
logger421 <wojteklotko3@gmail.com>
2022-12-20 18:36:02 +0100

    Added RandomQueue class

---
+++ Zadania10/random_queue.py
@@ -0,0 +1,26 @@
+import random
+
+
+class RandomQueue:
+    def __init__(self):
+        self.items = []
+
+    def insert(self, data):
+        self.items.append(data)
+
+    def remove(self):
+        max = len(self.items) - 1
+        idx = random.randint(0, max)
+        tmp = self.items[idx]
+        self.items[idx] = self.items[max]
+        self.items[max] = tmp
+        return self.items.pop()
+
+    def is_empty(self):
+        return not self.items
+
+    def is_full(self):
+        return False
+
+    def clear(self):
+        self.items.clear()
