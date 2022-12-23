36176b4529ea75f3bcbf0fa31a4708f085c03985
logger421 <wojteklotko3@gmail.com>
2022-12-23 16:18:30 +0100

    Added tests for further implementation.

--- Projekt/test_graph.py
+++ Projekt/test_graph.py
@@ -1,4 +1,43 @@
 import pytest
+import graph as g
+from edge import Edge
+
+
+def test_init():
+    gph = g.Graph()
+
+
+def test_create_vertices():
+    pass
+
+
+def test_add_edge():
+    pass
+
+
+def test_remove_edge():
+    pass
+
+
+def test_is_in_graph():
+    pass
+
+
+def test_print():
+    pass
+
+
+def test_sort_lists():
+    pass
+
+
+def test_bfs():
+    pass
+
+
+def test_dfs():
+    pass
+
 
 if __name__ == '__main__':
     pytest.main()
