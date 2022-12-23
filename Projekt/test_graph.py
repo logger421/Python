33151a593e17c690490dc6b9f8ff80178d48b01b
logger421 <wojteklotko3@gmail.com>
2022-12-23 22:01:58 +0100

    Updated tests

--- Projekt/test_graph.py
+++ Projekt/test_graph.py
@@ -4,31 +4,66 @@ from edge import Edge
 
 
 def test_init():
-    gph = g.Graph()
+    with pytest.raises(ValueError):
+        gph = g.Graph(-1)
 
-
-def test_create_vertices():
-    pass
+    gph = g.Graph(2)
+    assert gph.max_vertices == 2
+    for el in gph.graph:
+        assert el is None
 
 
 def test_add_edge():
-    pass
+    gph = g.Graph(2)
+
+    with pytest.raises(ValueError):
+        gph.add_edge(Edge(4, 5))
+
+    gph.add_edge(Edge(0, 1))
+    assert 1 in gph.graph[0]
+
+    gph.add_edge(Edge(0, 0))
+    assert 0 in gph.graph[0]
 
 
 def test_remove_edge():
-    pass
+    gph = g.Graph(2)
+
+    with pytest.raises(ValueError):
+        gph.remove_edge(Edge(0, 0))
+
+    gph.add_edge(Edge(0, 1))
+    assert 1 in gph.graph[0]
+
+    gph.remove_edge(Edge(0, 1))
+    assert not 1 in gph.graph[0]
 
 
 def test_is_in_graph():
-    pass
+    gph = g.Graph(2)
+    assert not gph.is_in_graph(Edge(0, 0))
 
-
-def test_print():
-    pass
+    gph.add_edge(Edge(0, 1))
+    assert gph.is_in_graph(Edge(0, 1))
 
 
 def test_sort_lists():
-    pass
+    gph = g.Graph(4)
+    gph.add_edge(Edge(0, 4))
+    gph.add_edge(Edge(0, 3))
+    gph.add_edge(Edge(0, 2))
+    gph.add_edge(Edge(0, 1))
+
+    gph.add_edge(Edge(4, 1))
+    gph.add_edge(Edge(4, 2))
+    gph.add_edge(Edge(4, 3))
+    gph.sort_lists()
+
+    assert gph.graph[0] == [1, 2, 3, 4]
+    assert gph.graph[4] == [1, 2, 3]
+    assert gph.graph[1] is None
+    assert gph.graph[2] is None
+    assert gph.graph[3] is None
 
 
 def test_bfs():
